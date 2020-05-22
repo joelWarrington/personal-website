0683523d3ba5a0d5117a1d56d0a334ee39cd9d52
joelWarrington <joelwarrington@gmail.com>
2020-05-21 23:38:41 -0600

    moved nav to layout

--- src/components/layout.tsx
+++ src/components/layout.tsx
@@ -5,9 +5,17 @@
  * See: https://www.gatsbyjs.org/docs/use-static-query/
  */
 
-import React from "react"
+import React, { useState } from "react"
 import PropTypes from "prop-types"
 import { useStaticQuery, graphql } from "gatsby"
+import {
+  Container,
+  Segment,
+  Sidebar,
+  Menu,
+  Icon,
+  Responsive,
+} from "semantic-ui-react"
 
 import Header from "./header"
 import "./layout.css"
@@ -22,19 +30,70 @@ const Layout = ({ children }) => {
       }
     }
   `)
+  const [width, setWidth] = useState(null)
+  const [sidebarOpen, setSidebar] = useState(false)
 
   return (
-    <>
-      <div
-        style={{
-          margin: `0 auto`,
-          maxWidth: 960,
-          padding: `0 1.0875rem 1.45rem`,
-        }}
-      >
-        {children}
-      </div>
-    </>
+    <Responsive
+      as={Sidebar.Pushable}
+      onUpdate={(e, { width }) => {
+        setWidth(width)
+        if (Responsive.onlyMobile.maxWidth <= width) setSidebar(false)
+      }}
+      fireOnMount
+    >
+      <Segment inverted textAlign="center" vertical>
+        {Responsive.onlyMobile.maxWidth >= width ? (
+          <>
+            <Sidebar
+              as={Menu}
+              animation="overlay"
+              direction="top"
+              onHide={() => setSidebar(false)}
+              vertical
+              visible={sidebarOpen}
+              width="thin"
+            >
+              <Menu.Item link onClick={() => setSidebar(false)}>
+                About
+              </Menu.Item>
+              <Menu.Item link onClick={() => setSidebar(false)}>
+                Projects
+              </Menu.Item>
+              <Menu.Item link onClick={() => setSidebar(false)}>
+                Blog
+              </Menu.Item>
+              <Menu.Item link onClick={() => setSidebar(false)}>
+                Connect
+              </Menu.Item>
+            </Sidebar>
+            <Container>
+              <Menu inverted pointing secondary size="large">
+                <Menu.Item position="right" onClick={() => setSidebar(true)}>
+                  <Icon name="sidebar" size="large" />
+                </Menu.Item>
+              </Menu>
+            </Container>
+          </>
+        ) : (
+          <Menu inverted secondary={true} size="large" borderless>
+            <Container>
+              <Menu.Item link position="right">
+                About
+              </Menu.Item>
+              <Menu.Item link>Projects</Menu.Item>
+              <Menu.Item link>Blog</Menu.Item>
+              <Menu.Item link>Connect</Menu.Item>
+            </Container>
+          </Menu>
+        )}
+      </Segment>
+      <Sidebar.Pusher dimmed={sidebarOpen}>
+        <Segment inverted textAlign="center" vertical>
+          {children}
+        </Segment>
+      </Sidebar.Pusher>
+    </Responsive>
   )
 }
 
