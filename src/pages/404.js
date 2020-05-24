14df372fbdaec4692aeaa58a4841b9655263bf4a
joelWarrington <joelwarrington@gmail.com>
2020-05-24 17:56:18 -0600

    updated default 404 page

--- src/pages/404.js
+++ src/pages/404.js
@@ -1,14 +1,21 @@
 import React from "react"
+import "animate.css/animate.min.css"
+import "semantic-ui-css/semantic.min.css"
+import { Container, Segment, Button, Icon } from "semantic-ui-react"
+import "./404.scss"
 
 import Layout from "../components/layout"
 import SEO from "../components/seo"
 
 const NotFoundPage = () => (
-  <Layout>
-    <SEO title="404: Not found" />
-    <h1>NOT FOUND</h1>
-    <p>You just hit a route that doesn&#39;t exist... the sadness.</p>
-  </Layout>
+  <Segment style={{ height: "100vh" }} inverted vertical>
+    <Container className="vert-center" inverted text textAlign="center">
+      <SEO title="404: Not found" />
+      <h1>404</h1>
+      <p>That page couldn't be found!</p>
+      <Button fluid icon="home" size="large" color="teal" as="a" href="/" />
+    </Container>
+  </Segment>
 )
 
 export default NotFoundPage
